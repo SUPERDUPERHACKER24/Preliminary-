0ec936d3fd76087578f6050b8667c6cdf51d11be
SUPERDUPERHACKER24 <SuperChintu24@gmail.com>
2016-09-26 06:56:52 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+
+// Notmal Java Hello World Program
+
+ public class HelloWorld {
+ 
+    // Main Method.
+   public static void main( String[] args ){
+    
+       System.out.println("Hello World !\n");
+        
+    }
+    
